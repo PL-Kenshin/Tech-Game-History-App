e2627ce0090bdd42a94867d5e364970728b7bf66
PL-Kenshin <mat-pt@wp.pl>
2023-06-13 22:12:21 +0200

    comment

--- src/components/table/table-row.jsx
+++ src/components/table/table-row.jsx
@@ -9,7 +9,7 @@ const TableRow = (props) => {
     <li style={styles} onClick={() => props.checkAnswer(props.question.isCorrect, props.currentKey)}
         className={"list-group-item " + (props.markedAnswer.key.includes(props.currentKey) ? props.markedAnswer.variant : '')}>{props.question.content}
     </li>
-   )
+   )//do zrobienia poprawienie kluczy i odpowiadających im kolorom poprawności odpowiedzi
 };
 
 export default TableRow;
