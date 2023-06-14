6c3b0b25848d42f7c204db1565dee070f6697530
PL-Kenshin <mat-pt@wp.pl>
2023-06-14 19:30:56 +0200

    ongoing

--- src/components/table/table-row.jsx
+++ src/components/table/table-row.jsx
@@ -6,11 +6,11 @@ const styles = {
 
 const TableRow = (props) => {
     console.log(props.markedAnswer)
-    console.log(props.currentKey, props.markedAnswer[props.currentKey+1]?.variant)
+    console.log(props.currentKey, props.markedAnswer.filter((obj) => obj.key === props.currentKey)[0]?.variant)
     console.log(props.markedAnswer.filter((obj) => obj.key === props.currentKey))
    return (
     <li style={styles} onClick={() => props.checkAnswer(props.question.isCorrect, props.currentKey)}
-        className={"list-group-item " + (props.markedAnswer.filter((obj) => obj.key === props.currentKey).length>0 ? props.markedAnswer[props.currentKey+1]?.variant : '')}>{props.question.content}
+        className={"list-group-item " + (props.markedAnswer.filter((obj) => obj.key === props.currentKey).length>0 ? props.markedAnswer.filter((obj) => obj.key === props.currentKey)[0]?.variant : '')}>{props.question.content}
     </li>
    )//do zrobienia poprawienie kluczy i odpowiadających im kolorom poprawności odpowiedzi
 };
